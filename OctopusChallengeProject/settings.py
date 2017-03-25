25554c81f54e7d853fcea2d112d7e6cbf5973ed8
swK <ric101@outlook.com>
2017-03-25 20:47:24 +0100

    ...9

--- OctopusChallengeProject/settings.py
+++ OctopusChallengeProject/settings.py
@@ -136,11 +136,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
