53930032894b84c645307b28468b5b58aaf85fdd
swK <ric101@outlook.com>
2017-03-25 20:20:26 +0100

    preparing for Heroku (3)

--- OctopusChallengeProject/settings.py
+++ OctopusChallengeProject/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = '6h+%9o4o2tc8k8t@tj1$3(*lm^g2tu^ru%gk7%3+$x4r%2_l$('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'sheltered-bastion-75881.herokuapp.com',
+]
 
 
 # Application definition
